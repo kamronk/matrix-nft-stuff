71bdb4364e60fb5f5d6b9ca4215bddabea4d1fcc
Kamron Kennedy <kamronkennedy@gmail.com>
2022-01-04 10:45:09 -0500

    Update main results table with more info and fa icons

--- docs/matrixnfts.js
+++ docs/matrixnfts.js
@@ -56,11 +56,10 @@ function findAndShow(ids){
 		$('#rarityResults').append(`
 			<tr>
 				<td>${ranking.tokenId}</td>
-				<td><strong>${ranking.rarestAttributeValue}</td>
-				<td><strong>${ranking.rarestAttributeRatio}</td>
+				<td>${ranking.rarestAttributeValue}</td>
 				<td><strong>${ranking.rank}</strong> / ${ranks.length} &nbsp <sup>Score: ${parseFloat("" + ranking.avgRatio).toFixed(5)}</sup></td>
-				<td><a class="button small" href="${ranking.image}" target="_blank">Download</a></td>
-				<td><a href="https://niftys.com/nft/0x39ceaa47306381b6d79ad46af0f36bc5332386f2/${ranking.tokenId}" target="_blank">Avatar # ${ranking.tokenId}</a></td>
+				<td><a class="button small" href="${ranking.image}" target="_blank"><i class="fas fa-download"></i></a></td>
+				<td><a class="button small" href="https://niftys.com/nft/0x39ceaa47306381b6d79ad46af0f36bc5332386f2/${ranking.tokenId}" target="_blank"><i class="fas fa-arrow-right"></i></a></td>
 			</tr>
 		`);
 	}
